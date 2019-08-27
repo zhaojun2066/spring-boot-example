45a0fedacf7a79f3a2d98029f7c4beb6d59d5669
zhaojun <zhaojun@300.cn>
2019-08-27 17:34:26 +0800

    update

--- example-028/src/main/java/com/jufeng/springboot/example028/App.java
+++ example-028/src/main/java/com/jufeng/springboot/example028/App.java
@@ -4,13 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
 @SpringBootApplication
 public class App implements CommandLineRunner {
 
     @Autowired
-    StringRedisTemplate stringRedisTemplate;
+    RedisTemplate redisTemplate;
 
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
@@ -18,8 +19,11 @@ public class App implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        stringRedisTemplate.opsForValue().set("hello","word");
-        String value = stringRedisTemplate.opsForValue().get("hello");
+        redisTemplate.opsForValue().set("hello","word");
+        String value = (String) redisTemplate.opsForValue().get("hello");
+        redisTemplate.delete("hello");
         System.out.println(value);
+         value = (String) redisTemplate.opsForValue().get("hello");
+        System.out.println("=="+value);
     }
 }
