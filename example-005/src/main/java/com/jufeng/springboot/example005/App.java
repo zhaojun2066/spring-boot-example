bdfa668f71ba92a27f82581492e2244a24a71a7c
zhaojun <zhaojun@300.cn>
2019-08-30 15:58:07 +0800

    update

--- example-005/src/main/java/com/jufeng/springboot/example005/App.java
+++ example-005/src/main/java/com/jufeng/springboot/example005/App.java
@@ -3,8 +3,9 @@ package com.jufeng.springboot.example005;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
-//@SpringBootApplication
+@SpringBootApplication
 public class App  implements CommandLineRunner {
     public static void main(String[] args) {
         System.out.println("开始");
@@ -15,5 +16,12 @@ public class App  implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         System.out.println("SpringApplication.run(.) 完成之前调用，此时bean 已经初始化完成了");
+    }
+
+    @Bean
+    public CommandLineRunner init() {
+      return args -> {
+          System.out.println("SpringApplication.run(.) 完成之前调用，此时bean 已经初始化完成了22222");
+      };
     }
 }
--- example-005/src/main/java/com/jufeng/springboot/example005/App2.java
+++ example-005/src/main/java/com/jufeng/springboot/example005/App2.java
@@ -6,7 +6,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class App2 implements ApplicationRunner {
     public static void main(String[] args) {
         System.out.println("开始");
