66561acf01878f68d26906580b53aba5f95a316e
zhaojun <zhaojun@300.cn>
2019-09-02 15:01:20 +0800

    添加测试

--- example-044/src/main/java/com/jufeng/springboot/example044/App.java
+++ example-044/src/main/java/com/jufeng/springboot/example044/App.java
@@ -1,11 +1,16 @@
 package com.jufeng.springboot.example044;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.JdbcTemplateAutoConfiguration;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.List;
 
 
 @SpringBootApplication(
@@ -15,11 +20,41 @@ import org.springframework.boot.autoconfigure.jdbc.JdbcTemplateAutoConfiguration
 public class App implements CommandLineRunner {
 
 
+    @Autowired
+    @Qualifier("oneJdbcTemplate")
+    private JdbcTemplate oneJdbcTemplate;
+
+
+    @Autowired
+    @Qualifier("twoJdbcTemplate")
+    private JdbcTemplate twoJdbcTemplate;
+
+
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
 
     @Override
     public void run(String... args) throws Exception {
+      List list =  oneJdbcTemplate.queryForList("select * from litemall_ad");
+        System.out.println(list);
+        List list2 =  twoJdbcTemplate.queryForList("select * from litemall_ad");
+        System.out.println(list2);
+        twoJdbcTemplate.queryForList("select * from litemall_ad");
+        twoJdbcTemplate.queryForList("select * from litemall_ad");
+        twoJdbcTemplate.queryForList("select * from litemall_ad");
+        twoJdbcTemplate.queryForList("select * from litemall_ad");
+        twoJdbcTemplate.queryForList("select * from litemall_ad");
+        twoJdbcTemplate.queryForList("select * from litemall_ad");
+        twoJdbcTemplate.queryForList("select * from litemall_ad");
+        twoJdbcTemplate.queryForList("select * from litemall_ad");
+        twoJdbcTemplate.queryForList("select * from litemall_ad");
+        twoJdbcTemplate.queryForList("select * from litemall_ad");
+        twoJdbcTemplate.queryForList("select * from litemall_ad");
+        twoJdbcTemplate.queryForList("select * from litemall_ad");
+        twoJdbcTemplate.queryForList("select * from litemall_ad");
+        twoJdbcTemplate.queryForList("select * from litemall_ad");
+        twoJdbcTemplate.queryForList("select * from litemall_ad");
+        twoJdbcTemplate.queryForList("select * from litemall_ad");
     }
 }
--- example-044/src/main/java/com/jufeng/springboot/example044/Conf.java
+++ example-044/src/main/java/com/jufeng/springboot/example044/Conf.java
@@ -1,11 +1,13 @@
 package com.jufeng.springboot.example044;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 
@@ -60,6 +62,20 @@ public class Conf {
     @Resource
     public PlatformTransactionManager tworansactionManager(DataSource twoDataSource){
         return new DataSourceTransactionManager(twoDataSource);
+    }
+
+
+    //JdbcTemplate主数据源
+   // @Primary
+    @Bean(name = "oneJdbcTemplate")
+    public JdbcTemplate oneJdbcTemplate(@Qualifier("oneDataSource") DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+
+    //JdbcTemplate第二个数据源
+    @Bean(name = "twoJdbcTemplate")
+    public JdbcTemplate twoJdbcTemplate(@Qualifier("twoDataSource") DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
     }
 
 }
